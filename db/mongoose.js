f0c9c48e461dfd39e57e58167f2c99e131bae72b
Aungthu772 <aungthuarun@gmail.com>
2018-02-25 02:16:40 +0630

    'init'

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const {URI} = require('../config/db');
 mongoose.Promise = global.Promise;
 
-
-mongoose.connect(URI);
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/nuratechDB');
 
   module.exports={
       mongoose
